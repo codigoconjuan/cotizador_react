7a83b3ccd48a3bd0f04ec22b905c3aad29555251
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-05 19:06:02 -0600

    primeros pasos

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = ({titulo}) => {
+    return ( 
+        <header>
+            <h1>{titulo}</h1>
+        </header>
+     );
+}
+ 
+export default Header;
