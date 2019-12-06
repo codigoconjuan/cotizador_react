883b3b0a0c5d9a7ee63a14347f0d8f5e891222c6
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-05 19:43:54 -0600

    styled component al button

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -20,6 +20,24 @@ const InputRadio = styled.input`
     margin: 0 1rem;
 `;
 
+const Boton = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover {
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
+
 const Formulario = () => {
     return ( 
         <form>
@@ -65,7 +83,7 @@ const Formulario = () => {
                 /> Completo
             </Campo>
 
-            <button type="button">Cotizar</button>
+            <Boton type="button">Cotizar</Boton>
         </form>
 
      );
