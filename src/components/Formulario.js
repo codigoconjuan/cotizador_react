538dfa7eead66e9302ea7dc7acd463bf36a8c980
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-05 19:39:02 -0600

    styled components al fomulario pt1

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,21 +1,41 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Campo = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`;
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+const InputRadio = styled.input`
+    margin: 0 1rem;
+`;
 
 const Formulario = () => {
     return ( 
         <form>
-            <div>
-                <label>Marca</label>
-                <select>
+            <Campo>
+                <Label>Marca</Label>
+                <Select>
                     <option value="">-- Seleccione --</option>
                     <option value="americano">Americano</option>
                     <option value="europeo">Europeo</option>
                     <option value="asiatico">Asiatico</option>
-                </select>
-            </div>
+                </Select>
+            </Campo>
 
-            <div>
-                <label>Año</label>
-                <select>
+            <Campo>
+                <Label>Año</Label>
+                <Select>
                     <option value="">-- Seleccione --</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -27,23 +47,23 @@ const Formulario = () => {
                     <option value="2014">2014</option>
                     <option value="2013">2013</option>
                     <option value="2012">2012</option>
-                </select>
-            </div>
+                </Select>
+            </Campo>
 
-            <div>
-                <label>Plan</label>
-                <input 
+            <Campo>
+                <Label>Plan</Label>
+                <InputRadio 
                     type="radio"
                     name="plan"
                     value="basico"
                 /> Básico
 
-                <input 
+                <InputRadio 
                     type="radio"
                     name="plan"
                     value="completo"
                 /> Completo
-            </div>
+            </Campo>
 
             <button type="button">Cotizar</button>
         </form>
