119e0fc6496cdf9db73a29a5fd6c0b86780338d9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-06 15:25:02 -0600

    validando form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -38,6 +38,15 @@ const Boton = styled.button`
     }
 `;
 
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width:100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
 
     const [ datos, guardarDatos ] = useState({
@@ -45,6 +54,7 @@ const Formulario = () => {
         year: '',
         plan: ''
     });
+    const [ error, guardarError ] = useState(false);
 
     // extraer los valores del state
     const { marca, year, plan } = datos;
@@ -57,8 +67,40 @@ const Formulario = () => {
         })
     }
 
+    // cuando el usuario presiona submit
+    const cotizarSeguro = e => {
+        e.preventDefault();
+
+        if(marca.trim() === '' || year.trim() === '' || plan.trim() === '') {
+            guardarError(true);
+            return;
+        }
+
+        guardarError(false);
+
+        // obtener la diferencia de años
+
+        // por cada año hay que restar el 3%
+
+        // Americano 15
+        // Asiatico 5%
+        // Europeo 30%
+
+
+        // Basíco aumenta 20%
+        // Completo 50%
+
+
+        // Total
+
+    }
+
     return ( 
-        <form>
+        <form
+            onSubmit={cotizarSeguro}
+        >
+            { error ? <Error>Todos los campos son obligatorios</Error>  : null }
+
             <Campo>
                 <Label>Marca</Label>
                 <Select
@@ -113,7 +155,7 @@ const Formulario = () => {
                 /> Completo
             </Campo>
 
-            <Boton type="button">Cotizar</Boton>
+            <Boton type="submit">Cotizar</Boton>
         </form>
 
      );
