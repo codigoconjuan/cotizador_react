b642952d46a36b5f9f75d215d09ef6a0acb169f9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-06 15:39:05 -0600

    calculando el modelo del auto

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from '@emotion/styled';
-import { obtenerDiferenciaYear } from '../helper';
+import { obtenerDiferenciaYear, calcularMarca } from '../helper';
 
 const Campo = styled.div`
     display: flex;
@@ -84,18 +84,15 @@ const Formulario = () => {
 
 
         // obtener la diferencia de años
-
         const diferencia = obtenerDiferenciaYear(year);
 
         // por cada año hay que restar el 3%
         resultado -= (( diferencia * 3 ) * resultado) / 100;
 
-        console.log(resultado)
-
         // Americano 15
         // Asiatico 5%
         // Europeo 30%
-
+        resultado = calcularMarca(marca) * resultado;
 
         // Basíco aumenta 20%
         // Completo 50%
--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,26 @@
+
+// obtiene la diferencia de años
 export function obtenerDiferenciaYear(year) {
     return new Date().getFullYear() - year;
+}
+
+// calcula el total a pagar segun la marca
+export function calcularMarca(marca) {
+    let incremento;
+
+    switch(marca) {
+        case 'europeo':
+            incremento = 1.30;
+            break;
+        case 'americano':
+            incremento = 1.15;
+            break;
+        case 'asiatico':
+            incremento = 1.05;
+            break
+        default:
+            break;
+    }
+
+    return incremento;
 }
