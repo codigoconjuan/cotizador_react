38e2edf097b54a4c364fce1e778bbc93a199a6d9
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-05 19:19:19 -0600

    styled components en app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Header from './components/Header';
 
+import styled from '@emotion/styled';
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+const ContenedorFormulario = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`
+
 function App() {
   return (
-    <Header 
-      titulo='Cotizador de Seguros'
-    />
+    <Contenedor>
+        <Header 
+          titulo='Cotizador de Seguros'
+        />
+
+        <ContenedorFormulario>
+            
+        </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
