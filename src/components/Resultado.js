9bf2d8f39286c9f686bb24530b93672f05ce324e
JuanDevWP <blackmaxxgdl18@hotmail.com>
2019-12-06 18:05:52 -0600

    agregando react-transition-group

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import { TransitionGroup, CSSTransition  } from 'react-transition-group';
 
 const Mensaje = styled.p`
     background-color: rgb(127, 224, 237);
@@ -34,7 +35,18 @@ const Resultado = ({cotizacion}) => {
             :  
                 (
                     <ResultadoCotizacion>
-                        <TextoCotizacion>El total es: $ {cotizacion}  </TextoCotizacion>
+                        <TransitionGroup
+                            component="p"
+                            className="resultado"
+                        >
+                            <CSSTransition
+                                classNames="resultado"
+                                key={cotizacion}
+                                timeout={{ enter: 500, exit: 500}}
+                            >
+                                <TextoCotizacion>El total es: $ {cotizacion}  </TextoCotizacion>
+                            </CSSTransition>
+                        </TransitionGroup>
                     </ResultadoCotizacion>
                 )
     )
